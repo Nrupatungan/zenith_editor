4b57113e4c8214a0a5d390d3e2cf20b765d1eebf
Dinesh Sake <dineshsake12345@gmail.com>
2025-08-06 22:58:39 +0530

    Database integration trial 2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,4 +11,4 @@
 import { PrismaClient } from '@/app/generated/prisma'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+export const prisma = new PrismaClient().$extends(withAccelerate())
