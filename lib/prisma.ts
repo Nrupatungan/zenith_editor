b581d823e724c74edd9db8690cd4d6e141681fca
Dinesh Sake <dineshsake12345@gmail.com>
2025-08-06 22:54:09 +0530

    Database integration trial 1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,14 @@
-import { PrismaClient } from "@/app/generated/prisma";
+// import { PrismaClient } from "@/app/generated/prisma";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+// export const prisma =
+//   globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+
+import { PrismaClient } from '@/app/generated/prisma'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient().$extends(withAccelerate())
